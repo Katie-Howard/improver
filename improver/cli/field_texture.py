71143456ec99d1804d7222400b2ae4dc40a08cb8
Kathryn.Howard <kathryn.howard@metoffice.gov.uk>
2021-02-22 15:06:23 +0000

    field texture diagnostic 0.75, texture threshold 0.01.

--- improver/cli/field_texture.py
+++ improver/cli/field_texture.py
@@ -40,7 +40,7 @@ def process(
     *,
     nbhood_radius: float = 20000.0,
     textural_threshold: float = 0.01,
-    diagnostic_threshold: float = 0.8125,
+    diagnostic_threshold: float = 0.75,
     model_id_attr: str = None,
 ):
 
