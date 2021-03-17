7d6491759943884a2f83333f239d7b13b0db1f68
Kathryn.Howard <kathryn.howard@metoffice.gov.uk>
2021-03-17 15:37:22 +0000

    field test 3 control 0.05.

--- improver/wxcode/wxcode_decision_tree.py
+++ improver/wxcode/wxcode_decision_tree.py
@@ -104,7 +104,8 @@ def wxcode_decision_tree():
             "threshold_condition": ">=",
             "condition_combination": "",
             "diagnostic_fields": [TEXTURE_PROB_ABOVE],
-            "diagnostic_thresholds": [(0.06, 1)],
+            "diagnostic_thresholds": [(0.05, 1)],
+#            "diagnostic_thresholds": [(0.06, 1)],
             "diagnostic_conditions": ["above"],
 #
         },
@@ -125,7 +126,8 @@ def wxcode_decision_tree():
             "threshold_condition": ">=",
             "condition_combination": "",
             "diagnostic_fields": [TEXTURE_PROB_ABOVE],
-            "diagnostic_thresholds": [(0.06, 1)],
+            "diagnostic_thresholds": [(0.05, 1)],
+#            "diagnostic_thresholds": [(0.06, 1)],
             "diagnostic_conditions": ["above"],
         },
 #
@@ -216,7 +218,8 @@ def wxcode_decision_tree():
             "threshold_condition": ">=",
             "condition_combination": "",
             "diagnostic_fields": [TEXTURE_PROB_ABOVE],
-            "diagnostic_thresholds": [(0.06, 1)],
+            "diagnostic_thresholds": [(0.05, 1)],
+#            "diagnostic_thresholds": [(0.06, 1)],
             "diagnostic_conditions": ["above"],
         },
         "heavy_snow_shower_in_vicinity": {
@@ -260,7 +263,8 @@ def wxcode_decision_tree():
             "threshold_condition": ">=",
             "condition_combination": "",
             "diagnostic_fields": [TEXTURE_PROB_ABOVE],
-            "diagnostic_thresholds": [(0.06, 1)],
+            "diagnostic_thresholds": [(0.05, 1)],
+#            "diagnostic_thresholds": [(0.06, 1)],
             "diagnostic_conditions": ["above"],
         },
         "heavy_rain_shower_in_vicinity": {
@@ -290,7 +294,8 @@ def wxcode_decision_tree():
             "threshold_condition": ">=",
             "condition_combination": "",
             "diagnostic_fields": [TEXTURE_PROB_ABOVE],
-            "diagnostic_thresholds": [(0.06, 1)],
+            "diagnostic_thresholds": [(0.05, 1)],
+#            "diagnostic_thresholds": [(0.06, 1)],
             "diagnostic_conditions": ["above"],
         },
         "drizzle_mist": {
