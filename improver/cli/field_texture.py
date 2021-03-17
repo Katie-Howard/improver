a383d1688e54bb948b9c291a86c906e4d5b668b8
Kathryn.Howard <kathryn.howard@metoffice.gov.uk>
2021-03-17 15:52:23 +0000

    field test 3 0.07.

--- improver/cli/field_texture.py
+++ improver/cli/field_texture.py
@@ -39,7 +39,7 @@ def process(
     cube: cli.inputcube,
     *,
     nbhood_radius: float = 20000.0,
-    textural_threshold: float = 0.06,
+    textural_threshold: float = 0.07,
 # just to know this is the right one to change.
     diagnostic_threshold: float = 0.8125,
     model_id_attr: str = None,
